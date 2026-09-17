51d69132523ada5316e6207a5075fd3e07691f20
Dan Madelo <dmadelo11@gmail.com>
2026-09-17 14:44:57 +0800

    feat: set the interface in Poppins, served from this server and cut to the latin subset

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -8,7 +8,8 @@
 <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
 <link rel="apple-touch-icon" href="{{ asset('images/logo.png') }}">
 
-@fonts
-
+{{-- Flux's @fonts directive is gone from here on purpose: it preloads the
+     Inter it ships with, and this app sets Poppins in app.css. Leaving it
+     in would fetch a typeface nothing renders. --}}
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
